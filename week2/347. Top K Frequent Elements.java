373f7b804dea2cb3e82bbf9d105a6c564d188ce6
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-07-28 16:21:00 +0530

    Create 347. Top K Frequent Elements.java

---
+++ week2/347. Top K Frequent Elements.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+            (a, b) -> countMap.get(a) - countMap.get(b)
+        );
+
+        for (int num : countMap.keySet()) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
+}
